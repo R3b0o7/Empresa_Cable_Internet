4a1f9366be03063c6d9012d0a05f06cb24659cdd
Luciano <dodaroluciano@gmail.com>
2022-07-16 15:40:36 -0300

    V1

--- src/GUI/VentanaAdministrador.java
+++ src/GUI/VentanaAdministrador.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+ad
 public class VentanaAdministrador extends JFrame implements ActionListener {
 
     //Botones superiores
