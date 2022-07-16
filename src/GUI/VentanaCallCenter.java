913b418afe2adfabb149fea3bf191cbdb3af16ae
Luciano <dodaroluciano@gmail.com>
2022-07-16 17:07:11 -0300

    V1

--- src/GUI/VentanaCallCenter.java
+++ src/GUI/VentanaCallCenter.java
@@ -18,8 +18,9 @@ public class VentanaCallCenter extends JFrame implements ActionListener {
     JButton botonListarServiciosPorEstado;
     JButton botonCerrarSesion;
     //Paneles
-    //JPanel menuPrincipal;
     JPanel altaCliente;
+    JPanel reservarServicio;
+    JPanel listarServicios;
 
     public VentanaCallCenter() {
         //Encabezado Ventana
@@ -30,11 +31,6 @@ public class VentanaCallCenter extends JFrame implements ActionListener {
                 "4. Introducción a la Orientación a Objetos\\TPO\\_Empresa Cable e Internet\\TPO\\src\\GUI\\Logo.png"); //crea un icono de la imagen
         this.setIconImage(image.getImage());
 
-        //Paneles --> (Ancho: 900,Alto: 600)
-        //menuPrincipal = new JPanel();
-        //menuPrincipal.setBackground(new Color(107,108,109));
-        //menuPrincipal.setBounds(0, 50,900,450);
-
         JPanel menuSuperior = new JPanel();
         menuSuperior.setBackground(new Color(169, 169, 169));
         menuSuperior.setBounds(0, 0,900,40);
@@ -44,8 +40,16 @@ public class VentanaCallCenter extends JFrame implements ActionListener {
         inicio.setBounds(0, 525,150,40);
 
         altaCliente = new JPanel();
-        altaCliente.setBackground(new Color(107,108,109));
+        altaCliente.setBackground(new Color(192, 18, 18));
         altaCliente.setBounds(0, 50,900,450);
+
+        reservarServicio = new JPanel();
+        reservarServicio.setBackground(new Color(0, 107, 210));
+        reservarServicio.setBounds(0, 50,900,450);
+
+        listarServicios = new JPanel();
+        listarServicios.setBackground(new Color(117, 227, 0));
+        listarServicios.setBounds(0, 50,900,450);
 
 
         //Botones Superiores
@@ -53,14 +57,17 @@ public class VentanaCallCenter extends JFrame implements ActionListener {
         botonReservarServicio = new JButton("Reservar servicio");
         botonReservarServicio.setHorizontalAlignment(JLabel.LEFT);
         botonReservarServicio.setBackground(Color.lightGray);
+        botonReservarServicio.addActionListener(this);
 
         botonAltaCliente = new JButton("Alta cliente");
         botonAltaCliente.setHorizontalAlignment(JLabel.CENTER);
         botonAltaCliente.setBackground(Color.lightGray);
+        botonAltaCliente.addActionListener(this);
 
         botonListarServiciosPorEstado = new JButton("Listar servicios por estado");
         botonListarServiciosPorEstado.setHorizontalAlignment(JLabel.RIGHT);
         botonListarServiciosPorEstado.setBackground(Color.lightGray);
+        botonListarServiciosPorEstado.addActionListener(this);
 
         //Boton Inferior
 
@@ -71,13 +78,25 @@ public class VentanaCallCenter extends JFrame implements ActionListener {
 
         //Agregar items a la ventana
 
-        //this.add(menuPrincipal);
+        this.add(altaCliente);
+        this.add(reservarServicio);
+        this.add(listarServicios);
         this.add(menuSuperior);
         menuSuperior.add(botonReservarServicio);
         menuSuperior.add(botonAltaCliente);
         menuSuperior.add(botonListarServiciosPorEstado);
+
         this.add(inicio);
         inicio.add(botonCerrarSesion);
+
+        //Menu Alta Cliente
+        altaCliente.setVisible(false);
+
+        //Menu Reservar Servicio
+        reservarServicio.setVisible(false);
+
+        //Menu Listar Servicios Por Estado
+        listarServicios.setVisible(false);
 
         //Ventana
         this.setSize(900,600);
@@ -92,6 +111,21 @@ public class VentanaCallCenter extends JFrame implements ActionListener {
         if(e.getSource()==botonCerrarSesion){
             this.dispose();
             new VentanaLogin();
+        }
+        if(e.getSource()==botonAltaCliente){
+            reservarServicio.setVisible(false);
+            listarServicios.setVisible(false);
+            altaCliente.setVisible(true);
+        }
+        if(e.getSource()==botonReservarServicio){
+            altaCliente.setVisible(false);
+            listarServicios.setVisible(false);
+            reservarServicio.setVisible(true);
+        }
+        if(e.getSource()==botonListarServiciosPorEstado){
+            altaCliente.setVisible(false);
+            reservarServicio.setVisible(false);
+            listarServicios.setVisible(true);
         }
     }
 }
